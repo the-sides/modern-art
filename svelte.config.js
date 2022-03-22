166f38f92a2acc5b6641f9c014e0201384d18da8
Jacob Sides <jsides@pyxl.com>
2022-03-21 19:23:10 -0700

    Revert "Netlify adapter, not that it seemed to be needed..."
    This reverts commit 2a5c8503dbc5037ebbd4af16c90f447c95f48acf.

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
