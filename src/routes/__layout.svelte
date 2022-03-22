d4dbd5d7ca86b288a9d9e5c1fcfdf1da07b48249
Jacob Sides <jsides@pyxl.com>
2022-03-21 19:36:00 -0700

    og:image fix?

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,9 @@
 	<meta property="og:title" content="nudibranch lovers anonymous"/>
     <meta property="og:description" content="A place to fuck around and love nudibranch"/>
     <meta property="og:site_name" content="Center of NLA"/>
-    <meta property="og:image" content="banner.jpg"/> 
+    <meta property="og:image" content="https://nudibranch-forever.netlify.app/banner.jpg"/> 
+	<meta property="og:image:width" content="1200"/>
+	<meta property="og:image:height" content="630"/>
 </svelte:head>
 <script>
 	import '../styles/tailwind.css';
