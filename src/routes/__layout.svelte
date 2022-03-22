7b2cf2f4f245a3a2832ebcbbf6cf3a6276e4e4b1
Jacob Sides <jsides@pyxl.com>
2022-03-21 19:27:02 -0700

    Meta data

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,10 @@
+<svelte:head>
+	<title>nudibranch lovers anonymous</title>
+	<meta property="og:title" content="nudibranch lovers anonymous"/>
+    <meta property="og:description" content="A place to fuck around and love nudibranch"/>
+    <meta property="og:site_name" content="Center of NLA"/>
+    <meta property="og:image" content="banner.jpg"/> 
+</svelte:head>
 <script>
 	import '../styles/tailwind.css';
 	import '../styles/main.css';
