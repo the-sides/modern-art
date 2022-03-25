3ca08ca456744d49367da3214a13df44a062e495
Jacob Sides <jsides@pyxl.com>
2022-03-24 21:04:15 -0700

    Meta: fix og image old domain

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,7 @@
 	<meta property="og:title" content="nudibranch lovers anonymous"/>
     <meta property="og:description" content="A place to fuck around and love nudibranch"/>
     <meta property="og:site_name" content="Center of NLA"/>
-    <meta property="og:image" content="https://nudibranch-forever.netlify.app/banner.jpg"/> 
+    <meta property="og:image" content="https://modern-fart.netlify.app/banner.jpg"/> 
 	<meta property="og:image:width" content="1200"/>
 	<meta property="og:image:height" content="630"/>
 </svelte:head>
