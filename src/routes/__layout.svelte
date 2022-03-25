3fb504b4572626cf20b2894527519ccada3a476d
Jacob Sides <jsides@pyxl.com>
2022-03-24 21:05:04 -0700

    Meta: more while I'm at it

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,8 @@
 <svelte:head>
 	<title>nudibranch lovers anonymous</title>
-	<meta property="og:title" content="nudibranch lovers anonymous"/>
-    <meta property="og:description" content="A place to fuck around and love nudibranch"/>
-    <meta property="og:site_name" content="Center of NLA"/>
+	<meta property="og:title" content="wire mother lovers anonymous"/>
+    <meta property="og:description" content="A place to fuck around and cuddle wires"/>
+    <meta property="og:site_name" content="Center of Wire Mothers"/>
     <meta property="og:image" content="https://modern-fart.netlify.app/banner.jpg"/> 
 	<meta property="og:image:width" content="1200"/>
 	<meta property="og:image:height" content="630"/>
